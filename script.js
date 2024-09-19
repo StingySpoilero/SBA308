d3f9c9556a66f55aefa9b542088e1499ab1f34f3
StingySpoilero <dalaujon@gmail.com>
2024-09-19 01:02:35 -0500

    SBA308

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const courseInfo = {
-    
+    "id": number,
+    "name": string,
 }
+const assignmentGroup = {
+    "id": number,
+  "name": string,
+  // the ID of the course the assignment group belongs to
+  "course_id": number,
+  // the percentage weight of the entire assignment group
+  "group_weight": number,
+  "assignments": [AssignmentInfo],
+}
+const LearnerSubmissions = {
+    
+    }
